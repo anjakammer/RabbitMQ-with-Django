de141aa78d9ce87f3e5eafff5cfd1bca65e9b517
Anja Kammer <post@anjakammer.de>
2017-10-09 03:47:56 +0200

    setup docker container for ipfs #3

--- webapp/tasks/views.py
+++ webapp/tasks/views.py
@@ -2,6 +2,7 @@ from .models import Task
 from .serializers import TaskSerializer, TaskSerializerGet
 from rest_framework import generics
 from django.core.files.storage import default_storage
+import ipfsapi
 
 
 class TaskListView(generics.ListCreateAPIView):
@@ -22,6 +23,11 @@ class TaskListView(generics.ListCreateAPIView):
         response = self.create(request, *args, **kwargs)
 
         data = request.FILES.get('resource')
+
+        api = ipfsapi.connect('127.0.0.1', 5001)
+        res = api.add('test.txt')
+
+        print('Hash:', res.get('Hash'))
 
         with default_storage.open('tmp/test_1.png', 'wb+') as destination:
             for chunk in data.chunks():
